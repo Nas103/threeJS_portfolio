e0894e7b5071aa36f84a5078891f03826c8a6553
Nas103 <tumelorhulani33@gmail.com>
2024-12-18 18:36:39 +0200

    favicon fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -61,7 +61,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I've done and learned so far.
+          What I've done and learnt so far.
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
           Courses | Experience.
