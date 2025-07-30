10745f67ca4f4f953a67189e24dbc165212fbe11
Nas103 <tumelorhulani33@gmail.com>
2025-07-30 21:45:54 +0200

    index

--- src/components/index.js
+++ src/components/index.js
@@ -9,26 +9,7 @@ import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 import Chat from './Chat';
 import ErrorBoundary from './ErrorBoundary';
-
-export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  Chat,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas,
-  ErrorBoundary
-};
-
 import Certifications from './Certifications';
-
 export {
   Hero,
   Navbar,
@@ -38,13 +19,15 @@ export {
   Works,
   Feedbacks,
   Contact,
+  Chat,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
   StarsCanvas,
-  Chat,
+  ErrorBoundary,
   Certifications
 };
+
 // src/components/index.js
 export { default as Navbar } from './Navbar';
 // Add this line:
